e1be38b905b71c5174770aea2fbdab1f17e35c2d
serIvanich <serivan1108@gmail.com>
2021-10-21 22:40:10 +0300

    prod reducer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "../features/header/Header";
 import {ProductsPage} from "../features/product/ProductsPage";
-import {MyShoppingPage} from "../features/myShoppingPage/MyShoppingPage";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ProductsPage />
-      <MyShoppingPage />
+      
     </div>
   );
 }
