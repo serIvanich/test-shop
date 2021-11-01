e490b72ac5af48494f050ceb26999b282fca2500
serIvanich <serivan1108@gmail.com>
2021-11-01 22:50:58 +0200

    added Router to cart

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './Header.module.css'
 
 export const Header: React.FC = () => {
 
     return (
-        <div>
-    header
+        <div className={s.container}>
+            <h2>ShopName</h2>
+            <div className={s.navCart}>
+                <NavLink to={'/cart'}> shopping cart </NavLink>
+            </div>
+
         </div>
     )
 }
--- src/features/shoppingCart/ShoppingCart.tsx
+++ src/features/shoppingCart/ShoppingCart.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './ShoppingCart.module.css'
 
 export const ShoppingCart: React.FC = () => {
 
     return (
         <div>
-            <div></div>
+            <div>there is not products in your shopping cart</div>
+            <div className={s.containerNavlinks}>
+                <NavLink to={'/'}>Go to start</NavLink>
+                <NavLink to={'/products'}>To shopping again</NavLink>
+            </div>
+
             <div></div>
 
         </div>
