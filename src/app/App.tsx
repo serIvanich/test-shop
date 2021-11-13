b7afe0763cb92d8145a70114ca20d1704c8b1581
serIvanich <serivan1108@gmail.com>
2021-11-13 23:48:12 +0200

    padding for own content

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import './App.css';
 import {Header} from "../features/header/Header";
 import {ProductsPage} from "../features/products/ProductsPage";
@@ -6,9 +6,6 @@ import {Context} from "../index";
 import {Route, Switch} from "react-router-dom";
 import {ShoppingCart} from "../features/shoppingCart/ShoppingCart";
 import {Main} from '../features/main/Main';
-import {actionProducts, ProductType} from "../features/products/products-reduser";
-import {getDatabase, onValue, ref} from "firebase/database";
-import {useActions} from "../utils/redux-utils";
 
 
 function App() {
@@ -18,12 +15,14 @@ function App() {
     return (
         <div className="App">
             <Header/>
+            <div className={'content'}>
+                <Switch>
+                    <Route exact path={'/'} render={() => <Main/>}/>
+                    <Route path={'/products'} render={() => <ProductsPage/>}/>
+                    <Route path={'/cart'} render={() => <ShoppingCart/>}/>
+                </Switch>
+            </div>
 
-            <Switch>
-                <Route exact path={'/'} render={() => <Main/>}/>
-                <Route path={'/products'} render={() => <ProductsPage/>}/>
-                <Route path={'/cart'} render={() => <ShoppingCart/>}/>
-            </Switch>
 
         </div>
     );
